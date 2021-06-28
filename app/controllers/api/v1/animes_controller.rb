2eedfb657924677c764408c34fae53b3fc0c4031
Akatsuki507 <alfonso.alvarado.v31@gmail.com>
2021-06-27 21:49:32 -0400

    add large anime list endpoint

--- app/controllers/api/v1/animes_controller.rb
+++ app/controllers/api/v1/animes_controller.rb
@@ -1,9 +1,25 @@
 require 'json'
 class Api::V1::AnimesController < ApplicationController
+  after_action :cors_set_access_control_headers
+
   def trending
     file = File.read('lib/trending_animes.json')
     @animes = JSON.parse(file)
-    p @animes
     render json: @animes
+  end
+
+  def index
+    file = File.read('lib/large_list.json')
+    @animes = JSON.parse(file)
+    render json: @animes
+  end
+
+  private
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
